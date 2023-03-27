5769c3ae106d94451022a79aec7b2513e9d31da4
DESKTOP-LKMSUIL\zgz68 <zgz682000@163.com>
2023-03-27 11:16:17 +0800

    解决ipa里面包含手表时取出手表包名的bug

--- src/upload-hook.ts
+++ src/upload-hook.ts
@@ -3,6 +3,9 @@ import payload from "payload";
 import { PayloadRequest } from "payload/types";
 import {IpaReader, ApkReader}  from "app-package-parser";
 
+const PLIST_REG = new RegExp("payload/[^/]+?.app/info.plist$", "i");
+const PROVISION_REG = /payload\/.+?\.app\/embedded.mobileprovision/;
+
 export const afterAPKUpload = async(args: {
     doc: any;
     req: PayloadRequest;
@@ -56,7 +59,7 @@ export const afterIPAUpload = async (args: {
     let ipaInfo: any;
     try {
         let ipaReader = new IpaReader(ipaRenamedPath);
-        ipaInfo = await ipaReader.parse();
+        ipaInfo = await ipaReader.parse([PLIST_REG, PROVISION_REG]);
     } catch (error) {
         throw new Error("ipa parse failure: " + ((error as Error).message || error));
     }
